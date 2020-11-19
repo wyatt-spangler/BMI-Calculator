4fd61ee7a1b3d9c4cc2e9224481bc9a97cb227e2
Wyatt Spangler <wyatt.spa@outlook.com>
2020-11-19 01:18:39 -0500

    part 2

--- BMI Calculator/MainWindow.xaml.cs
+++ BMI Calculator/MainWindow.xaml.cs
@@ -20,11 +20,26 @@ namespace BMI_Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double BMI;
+        double height;
+        double weight;
+        public class Customer
+        {
+            public string lastName { get; set; }
+            public string firstName { get; set; }
+            public string phoneNumber { get; set; }
+            public int heightInches { get; set; }
+            public int weightLbs { get; set; }
+            public int custBMI { get; set; }
+            public string statusTitle { get; set; }
+
+            
+        }
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        #region Part 1
         private void ClearBtn_Click(object sender, RoutedEventArgs e)
         {
             xPhoneBox.Text = "";
@@ -37,6 +52,37 @@ namespace BMI_Calculator
         private void ExitBtn_Click(object sender, RoutedEventArgs e)
         {
             Environment.Exit(0);
+        }
+        #endregion
+
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            height = Convert.ToDouble(xHeightInchesBox.Text);
+            weight = Convert.ToDouble(xWeightLbsBox.Text);
+
+            BMI = (weight / Math.Pow(height, 2)) * 703;
+            xYourBMIResults.Text = Convert.ToString(BMI);
+
+            if (BMI < 18.5)
+            {
+                //under weight
+                xBMIMessage.Text = "According to CDC.gov you are under weight.";
+            }
+            else if (BMI > 29.9)
+            {
+                //obese
+                xBMIMessage.Text = "According to CDC.gov you are obese.";
+            }
+            else if (BMI > 24.9)
+            {
+                //over weight
+                xBMIMessage.Text = "According to CDC.gov you are over weight.";
+            }
+            else
+            {
+                //normal
+                xBMIMessage.Text = "According to CDC.gov you are the normal weight.";
+            }
         }
     }
 }
